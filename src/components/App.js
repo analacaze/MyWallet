757aa9ebf50056fb2a38c3181c5ce92fd57f7977
analacaze <anaclaralacaze@gmail.com>
2020-12-02 21:46:29 -0300

    Route config

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Main from './Main';
 
 export default function App(){
-
+    return (
+        <Router>
+            <Switch>                
+                <Route path='/'>
+                    <Main />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
